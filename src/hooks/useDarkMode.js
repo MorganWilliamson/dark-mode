765603b703a8a39f220d5d506c8c092523602f26
MorganWilliamson <mwilliamson7695@gmail.com>
2020-10-28 17:30:31 -0500

    MVP completed!

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,6 +2,7 @@ import React from "react";
 import useLocalStorage from "./useLocalStorage";
 
 export const useDarkMode = (key, initialValue) => {
+    //remember to keep values generic. This is meant to be reusable.
     const [value, setValue] = useLocalStorage(key, initialValue);
     return [value, setValue]
 }
